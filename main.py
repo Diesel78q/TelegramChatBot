cf65dffce200ad1299dd45103fa75f9db37b07d5
Diesel78q <130408732+Diesel78q@users.noreply.github.com>
2023-06-03 02:20:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import openai
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+bot = telebot.TeleBot(os.getenv("TELEKEY"))
+
+
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+    )
+    bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+
+
+bot.polling()
